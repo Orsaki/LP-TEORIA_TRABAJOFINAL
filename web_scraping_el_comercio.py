8017eca1425419c517264b0edff7e6c94d821648
lazaropamela <studypamlove@gmail.com>
2025-12-20 16:21:37 -0500

    feat: implementar scraping de El Comercio para sección judiciales

--- web_scraping_el_comercio.py
+++ web_scraping_el_comercio.py
@@ -1,5 +1,13 @@
-# Librerias necesarias
+# Librerias necesarias 
 import requests
 from bs4 import BeautifulSoup
 import csv
+#EL COMERCIO - PERU
+URL_WEB = "https://elcomercio.pe/lima/judiciales/" # Sección con temas policiales/delitos
+NOMBRE_ARCHIVO = "noticias_elcomercio_filtradas.csv"
 
+PALABRAS_CLAVE = ["robo", "asalto", "delincuencia", "policía", "crimen", "sicario", "balacera", "muerte", "asesinato", "captura", "extorsión"]
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+}
