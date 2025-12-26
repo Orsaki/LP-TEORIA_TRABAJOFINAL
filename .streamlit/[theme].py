041d461b5d95d82fdddfa5f939d2216ca09d24e2
Orsaki <165319696+Orsaki@users.noreply.github.com>
2025-12-26 08:45:35 -0500

    agregando carpetas de streamlit

---
+++ .streamlit/[theme].py
@@ -0,0 +1,7 @@
+[theme]
+primaryColor = "#D32F2F"          # Rojo intenso (Alerta / Urgencia)
+backgroundColor = "#FFFFFF"       # Blanco puro (Estilo periódico/limpio)
+secondaryBackgroundColor = "#F0F2F6" # Gris muy suave (Para la barra lateral)
+textColor = "#262730"             # Negro casi puro (Lectura fácil)
+font = "sans serif"               # Fuente estándar moderna
+base = "light"                    # Asegura que el fondo sea claro por defecto
